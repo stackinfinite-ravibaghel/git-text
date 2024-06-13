ee3f60f36978cc9256e16a2bb33360dd47fa3179
stackinfinite-ravibaghel <ravi.baghel@stackinfinite.com>
2024-06-13 14:50:22 +0530

    Apply useRouter & Create SignUp - Dashboard file's

---
+++ src/app/Dashboard/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Dashboard() {
+  
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+            Welcome to Dashboard Section
+            
+    </main>
+  );
+}
